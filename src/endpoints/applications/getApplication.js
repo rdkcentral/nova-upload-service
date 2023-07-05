233b31b909d09257cbe097625e1c38505bc24070
Uğur Aslan <ugur@uguraslan.net>
2023-07-05 11:38:39 +0200

    WIP: express versions of app endpoints + create user fix

--- src/endpoints/applications/getApplication.js
+++ src/endpoints/applications/getApplication.js
@@ -3,13 +3,16 @@ const errorResponse = require('../../helpers/errorResponse')
 
 module.exports = async (req, res) => {
   try {
-    const data = await ApplicationModel.find({status: 'active'}, null, { sort: { createdAt: -1 } })
+    const result = await ApplicationModel.findOne({ _id: req.params.id, status: 'active' })
+    if (!result) {
+      return res.sendStatus(404)
+    }
     res.json({
-      data,
+      data : result,
       status: 'success'
     })
-  } catch (e){
+  } catch(e) {
     console.error(e)
-    errorResponse.send(res, 'applicationList failed', e)
+    errorResponse.send(res, 'applicationGet failed', e)
   }
 }
