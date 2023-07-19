8b98864eb53bb732fe38c9ac2aa311e67146dde1
Michiel van der Geest <hello@michielvandergeest.com>
2023-07-19 21:06:55 +0200

    Added servless-express setup. WIP and not fully tested. template.yaml needs configuration.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const PORT = process.env.NODE_PORT || 3000
 
 // create express app
 const app = express()
@@ -9,21 +8,17 @@ app.use(cors())
 app.use(bodyParser.json({ limit: process.env.MAX_REQUEST_BODY_SIZE }))
 
 // user paths
-app.use('/v1/login', require('./routes/login'))
-app.use('/v1/users', require('./routes/users'))
+app.use('/login', require('./routes/login'))
+app.use('/users', require('./routes/users'))
 
 // application paths
-app.use('/v1/applications', require('./routes/applications'))
+app.use('/applications', require('./routes/applications'))
 
 // http 404 error handler
 app.use((req, res) => {
   res.status(404).json({
     message: 'Not Found',
   })
-})
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
 })
 
 module.exports = app
