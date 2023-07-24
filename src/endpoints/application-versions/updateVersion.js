3951569166bf6dd8dfcc01f739249eb26118ee34
Michiel van der Geest <hello@michielvandergeest.com>
2023-07-24 21:52:25 +0200

    Added basic API endpoint for updating an application version.

--- src/endpoints/application-versions/updateVersion.js
+++ src/endpoints/application-versions/updateVersion.js
@@ -1,6 +1,31 @@
+const ApplicationVersionModel = require('../../models/ApplicationVersion').model
+const errorResponse = require('../../helpers/errorResponse')
+
 module.exports = async (req, res) => {
-  res.status(200).json({
-    data: 'Update Application Version',
-    status: 'success',
-  })
+  try {
+    const applicationVersion = await ApplicationVersionModel.findOne({
+      application: req.params.applicationId,
+      _id: req.params.id,
+    })
+
+    if (applicationVersion) {
+      for (const key in req.body) {
+        if (
+          key !== 'id' &&
+          key in applicationVersion &&
+          applicationVersion[key] !== req.body[key]
+        ) {
+          applicationVersion.set(key, req.body[key])
+        }
+      }
+      return res.status(200).json({
+        data: applicationVersion.toObject(),
+        status: 'success',
+      })
+    } else {
+      return errorResponse.send(res, 'Application version not found')
+    }
+  } catch (e) {
+    return errorResponse.send(res, 'applicationVersionUpdate failed', e)
+  }
 }
