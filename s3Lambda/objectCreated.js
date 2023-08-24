14411c9f9a4cec0d8d320d3275910abaabd41873
Uğur Aslan <ugur@uguraslan.net>
2023-08-24 14:26:10 +0200

    added the lambda function for s3 object created event

--- s3Lambda/objectCreated.js
+++ s3Lambda/objectCreated.js
@@ -1,63 +1,68 @@
+const AWS = require('aws-sdk')
+const s3 = new AWS.S3({
+  endpoint: process.env.AWS_ENDPOINT_URL_S3 || '', // Minio endpoint
+  s3ForcePathStyle: true, // Use path-style URLs
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+})
+const unzipper = require('unzipper')
+
+// eslint-disable-next-line no-unused-vars
 exports.handler = async function (event, context) {
-  const bucket = event.Records[0].s3.bucket.name
-  const key = decodeURIComponent(
-    event.Records[0].s3.object.key.replace(/\+/g, ' ')
-  )
-  /*
-  Sample event record:
+  try {
+    // Get the bucket and key from the S3 event
+    const { bucket, object } = event.Records[0].s3
+    const zipFileName = object.key.split('/').pop()
 
-  {
-    "Records": [
-      {
-        "eventVersion": "2.0",
-        "eventSource": "minio:s3",
-        "awsRegion": "",
-        "eventTime": "2023-08-24T08:36:23.013Z",
-        "eventName": "s3:ObjectCreated:Put",
-        "userIdentity": {
-          "principalId": "myAccessKey"
-        },
-        "requestParameters": {
-          "principalId": "myAccessKey",
-          "region": "",
-          "sourceIPAddress": "172.18.0.5"
-        },
-        "responseElements": {
-          "content-length": "0",
-          "x-amz-request-id": "177E44D30262FC78",
-          "x-minio-deployment-id": "2ca44632-7037-4164-bfe5-ff4a8f1d0865",
-          "x-minio-origin-endpoint": "http://172.18.0.5:9000"
-        },
-        "s3": {
-          "s3SchemaVersion": "1.0",
-          "configurationId": "Config",
-          "bucket": {
-            "name": "ugap",
-            "ownerIdentity": {
-              "principalId": "myAccessKey"
-            },
-            "arn": "arn:aws:s3:::ugap"
-          },
-          "object": {
-            "key": "deneme/Screenshot 2023-07-04 at 03.23.45.png",
-            "size": 257858,
-            "eTag": "bfc2a8650585bedaffa3fc617c9e22f2",
-            "contentType": "image/png",
-            "userMetadata": {
-              "content-type": "image/png"
-            },
-            "sequencer": "177E44D302CE33BC"
-          }
-        },
-        "source": {
-          "host": "172.18.0.5",
-          "port": "",
-          "userAgent": "MinIO (linux; amd64) minio-go/v7.0.34"
+    // Check if the object is a zip file
+    if (!object.key.endsWith('.zip')) {
+      console.log('Not a zip file, skipping.')
+      return
+    }
+
+    // Fetch the zip file from S3
+    const params = {
+      Bucket: bucket.name,
+      Key: object.key,
+    }
+
+    const zipFile = s3
+      .getObject(params)
+      .createReadStream()
+      .pipe(unzipper.Parse({ forceStream: true }))
+
+    const promises = []
+
+    for await (const entry of zipFile) {
+      const fileName = entry.path
+      const destinationPath =
+        object.key.replace(/^uploads/, 'apps').replace(`/${zipFileName}`, '') +
+        '/' +
+        fileName
+      console.log(`Extracting ${fileName} to ${destinationPath}`)
+
+      const type = entry.type
+      if (type === 'File') {
+        const uploadParams = {
+          Bucket: bucket.name,
+          Key: destinationPath,
+          Body: entry,
         }
-      }
-    ]
-  }
-*/
 
-  return 'Hello World'
+        promises.push(s3.upload(uploadParams).promise())
+      } else {
+        entry.autodrain()
+      }
+    }
+
+    await Promise.all(promises)
+      .then(function () {
+        console.log('Successfully extracted zip file.')
+      })
+      .catch(function (err) {
+        console.error('Error extracting zip file:', err)
+      })
+  } catch (error) {
+    console.error('Error:', error)
+  }
 }
