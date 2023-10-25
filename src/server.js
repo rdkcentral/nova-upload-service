afd8dda850acc0ad70b063855bd197d702c33ac2
Uğur Aslan <ugur@uguraslan.net>
2023-10-25 11:38:25 +0200

    added PORT env. variable for EB compatibility

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
-const PORT = process.env.NODE_PORT || 3000
+// Elastic Beanstalk works with PORT env variable
+const PORT = process.env.PORT || process.env.NODE_PORT || 3000
 
 const app = require('./app.js')
 
