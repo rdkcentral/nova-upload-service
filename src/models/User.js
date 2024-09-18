8907f6ed1e60bc8618fd1c3661e51db17de79a92
paul van der windt <p.van.der.windt@metrological.com>
2024-09-18 13:59:52 +0200

    naming fix

--- src/models/User.js
+++ src/models/User.js
@@ -118,7 +118,9 @@ UserSchema.methods.getCurrentPasswordObject = function () {
 }
 
 UserSchema.methods.isExpired = function (otp) {
-  const days = parseInt(otp ? process.env.OTP_VALID_FOR : process.env.PASSWORD_VALID_FOR)
+  const seconds = parseInt(
+    otp ? process.env.OTP_VALID_FOR : process.env.PASSWORD_VALID_FOR
+  )
   const expireDate = new Date(this.getCurrentPasswordObject().passwordUpdated)
   expireDate.setSeconds(expireDate.getSeconds() + seconds)
 
